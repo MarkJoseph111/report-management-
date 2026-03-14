b6545707d17847eac780dc2102425a6826dfff09
BEES10 <166887222+BEES10@users.noreply.github.com>
2026-03-15 05:15:35 +0800

    Refactor database connection to use environment variables

--- config.php
+++ config.php
@@ -1,15 +1,11 @@
 <?php
-
-
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "ars_db"; // updated to use ARS database
+$host     = getenv("DB_HOST");
+$user     = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
+$database = getenv("DB_NAME");
 
 $conn = new mysqli($host, $user, $password, $database);
-
 if ($conn->connect_error) {
-    die("Connection failed: ". $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
