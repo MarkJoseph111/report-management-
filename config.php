aad7438f301304009b084607de86dc7405bcf1fe
BEES10 <166887222+BEES10@users.noreply.github.com>
2026-03-15 07:00:05 +0800

    Add files via upload

--- config.php
+++ config.php
@@ -1,11 +1,15 @@
 <?php
-$host     = getenv("DB_HOST");
-$user     = getenv("DB_USER");
-$password = getenv("DB_PASSWORD");
-$database = getenv("DB_NAME");
+
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "ars_db"; // updated to use ARS database
 
 $conn = new mysqli($host, $user, $password, $database);
+
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: ". $conn->connect_error);
 }
+
 ?>
